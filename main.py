5124765eb1f3d42b29d26e7575571f58d8234f96
nullspechattacks <ownsnull@gmail.com>
2025-01-28 01:07:15 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+import time
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+
+bot = Bot(token="7413805589:AAGy3U8bylU8AOYIXOVoyrsODnhvCIw5oUI")
+dp = Dispatcher()
+
+
+@dp.message(Command("hello"))
+async def hello(message: types.Message):
+    start_time = time.time()
+    await message.answer("Hello, World!")
+    print(time.time() - start_time)
+
+
+async def main():
+    await dp.start_polling(bot) 
+
+
+asyncio.run(main())
